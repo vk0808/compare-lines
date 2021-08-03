092e9c7c62642d35dbcd0b464e05a94c2737795f
vk0808 <vk4765712@gmail.com>
2021-08-03 19:22:45 +0530

    feat: cal length

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ namespace LineComparsion
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparsion Computation Program");
+
+            Random rand = new Random();
+            
+            /// Line 1 co-ordinates
+            int x1 = rand.Next(0, 20);
+            int x2 = rand.Next(0, 20);
+            int y1 = rand.Next(0, 20);
+            int y2 = rand.Next(0, 20);
+
+            /// Calculate length
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
+
+            Console.WriteLine("(" + x1 + ", " + y1 + ")");
+            Console.WriteLine("(" + x2 + ", " + y2 + ")");
+            Console.WriteLine("Length: " + length);
         }
     }
 }
