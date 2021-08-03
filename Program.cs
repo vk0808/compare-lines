01354150602ae47eacb03040e494e42a198b2c22
vk0808 <vk4765712@gmail.com>
2021-08-03 18:01:17 +0530

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparsion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparsion Computation Program");
+        }
+    }
+}
